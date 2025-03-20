95966c26302884caed101d51da775ee6a8b66ea2
Konstantin <freetokdevelop@gmail.com>
2025-03-21 01:34:26 +0300

    working with video

--- test_comparison.py
+++ test_comparison.py
@@ -9,11 +9,8 @@ import cv2
 
 from PIL import Image
 
-import random
-
-
 # Load a model
-model = YOLO("yolo11n.pt")  # load an official model
+model = YOLO("yolo11n.pt", verbose=False)  # load an official model
 # model = YOLO("path/to/best.pt")  # load a custom model
 
 # Загрузка ResNet для извлечения признаков
@@ -30,27 +27,26 @@ transform = transforms.Compose([
 
 def get_detections(results):
     if not results or len(results) == 0: return None
-    print(f'results {len(results)}')
 
     res = []
     for result in results[0]:
         res.append(result.boxes.xyxy.tolist()[0])
-        print(f'result xyxy {result.boxes.xyxy.tolist()[0]}')
     
     return res
 
+index = 0
 # Функция для извлечения объектов
 def crop_objects(image, detections):
+    global index
     if len(detections) == 0: return None
 
     cropped_images = []
     for detection in detections:
-        print(f'detection {detection}')
         xmin, ymin, xmax, ymax = detection
         cropped_image = image.crop((xmin, ymin, xmax, ymax))
         cropped_images.append(cropped_image)
         
-        cropped_image.save(f"cropped_image_{random.randint(0, 100000)}.jpg")
+        index += 1
 
     return cropped_images
 
@@ -62,7 +58,7 @@ def get_feature_vector(image):
     return features.squeeze().numpy()
 
 # Функция для сравнения feature vectors
-def compare_features(features1, features2, threshold=0.1):
+def compare_features(features1, features2, threshold=0.75):
     for i, f1 in enumerate(features1):
         for j, f2 in enumerate(features2):
             similarity = cosine_similarity(f1.reshape(1, -1), f2.reshape(1, -1))
@@ -72,55 +68,65 @@ def compare_features(features1, features2, threshold=0.1):
     return False
 
 def compare_images(img1, img2):
+    if not img1 or not img2: return False
+
     # Predict with the model
-    results1 = model(img1)  # predict on an image
-    results2 = model(img2)  # predict on an image
+    results1 = model(img1, verbose=False)  # predict on an image
+    results2 = model(img2, verbose=False)  # predict on an image
+
+    if len(results1) == 0 or len(results2) == 0: return False
 
     detections1 = get_detections(results1)
     detections2 = get_detections(results2)
+
+    if len(detections1) == 0 or len(detections2) == 0: return False
 
     # Извлечение объектов
     cropped_images1 = crop_objects(img1, detections1)
     cropped_images2 = crop_objects(img2, detections2)
 
+    if len(cropped_images1) == 0 or len(cropped_images2) == 0: return False
+
     # Извлечение feature vectors
     features1 = [get_feature_vector(img) for img in cropped_images1]
     features2 = [get_feature_vector(img) for img in cropped_images2]
+
+    if len(features1) == 0 or len(features2) == 0: return False
 
     # Сравнение объектов
     return compare_features(features1, features2)
 
 
 img1 = Image.open('Input/A60/image1.jpg')
-img2 = Image.open('Input/A60/video.jpg')
+# img2 = Image.open('Input/A60/video.jpg')
 
-compare_images(img1, img2)
-
-
+# compare_images(img1, img2)
 
 
+# Загрузка видео
+video_path = 'Input/A60/video.mp4'
+cap = cv2.VideoCapture(video_path)
 
-# # Загрузка видео
-# video_path = 'video.mp4'
-# cap = cv2.VideoCapture(video_path)
+# Параметры для извлечения кадров
+frame_rate = 1  # 1 кадр в секунду
+frame_count = 0
+fps = int(cap.get(cv2.CAP_PROP_FPS))  # Получаем FPS видео
 
-# # Параметры для извлечения кадров
-# frame_rate = 1  # 1 кадр в секунду
-# frame_count = 0
-# fps = int(cap.get(cv2.CAP_PROP_FPS))  # Получаем FPS видео
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
 
-# while cap.isOpened():
-#     ret, frame = cap.read()
-#     if not ret:
-#         break
+    # Извлечение кадров с заданной частотой
+    if frame_count % (fps * frame_rate) == 0:
+        # Преобразование кадра в PIL Image
+        frame_pil = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 
-#     # Извлечение кадров с заданной частотой
-#     if frame_count % (fps * frame_rate) == 0:
-#         # Преобразование кадра в PIL Image
-#         frame_pil = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        if compare_images(img1, frame_pil):
+            print(frame_count / fps)
+            frame_pil.save(f'frame_{index}.jpg')
+            index += 1
 
-#         compare_features(img1, frame_pil)
+    frame_count += 1
 
-#     frame_count += 1
-
-# cap.release()
+cap.release()
